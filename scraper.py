6f388d378981b2834fa8cf48e929caa207f4c7b6
Elif Ölmez <elifolmez@Elifs-MacBook-Air.local>
2020-03-01 07:47:55 -0500

    prettier code

--- scraper.py
+++ scraper.py
@@ -1,20 +1,22 @@
 from selenium import webdriver
 
 with webdriver.Chrome() as driver:
+  
   base_url = "https://apkpure.com/app"
 
-  driver.get("https://apkpure.com/app")
-  # apps are web elements
+  driver.get(base_url)
+
+  # driver returns web elements
   apps = driver.find_elements_by_class_name("category-template-down")
   download_links = []
 
   for web_elt in apps:
-    anchr = web_elt.find_element_by_tag_name('a')
-    dl_link = anchr.get_attribute("href")
-    download_links.append(dl_link)
+    anchor = web_elt.find_element_by_tag_name('a')
+    l = anchor.get_attribute("href")
+    download_links.append(l)
   
-  
-  for applink in download_links:
-    driver.get(applink)
+  # click on each payload
+  for l in download_links:
+    driver.get(l)
 
 
